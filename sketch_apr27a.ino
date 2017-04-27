83a91adfeb4d17a40b33a6cddf08237532f89f2a
escri11 <escri11@yahoo.es>
2017-04-27 13:21:56 +0200

    Delay afegit, map's borrats

--- sketch_apr27a.ino
+++ sketch_apr27a.ino
@@ -1,24 +1,21 @@
 int led = 13;
-double analog_data = 0; 
+double analog_data = 0;
 double temp = 0;
 double control_led;
 
 void setup () {
   Serial.begin(9600);
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);
 
 }
 
 void loop () {
   analog_data = analogRead(3);
   temp = (analog_data*0.013)+17.027;
-  //delay(500);
-  //control_led = map(temp, 27, 27.6, 0, 256);
-  //control_led = map(temp, 22.6, 23, 256, 0);
+  delay(500);
   if (temp > 27) {digitalWrite(led, 1);}
   else if (temp < 23.5) {digitalWrite(led, 1);}
   else {digitalWrite(led, 0);}
-  
+
   Serial.println(temp);
 }
-
